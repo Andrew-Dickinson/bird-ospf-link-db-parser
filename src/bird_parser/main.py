3a26f3835db5bb8dca015fbbfa967c0f5bbb66af
Andrew Dickinson <Andrew-Dickinson@users.noreply.github.com>
2023-04-29 03:02:12 -0400

    Add output schema validation

--- src/bird_parser/main.py
+++ src/bird_parser/main.py
@@ -1,12 +1,15 @@
 import json
+import os.path
 import sys
 
 from bird_parser.parse_bird_output import parse_ospf_state_all
 
+from jsonschema import validate
+
 
 def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} OSPF_LINK_DB_TXT_FILE")
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} OSPF_LINK_DB_TXT_FILE [--no-validate]")
         sys.exit(2)
 
     file_name = sys.argv[1]
@@ -17,6 +20,12 @@ def main():
             input_str = input_file.read()
 
     link_db = parse_ospf_state_all(input_str)
+
+    if len(sys.argv) < 3 or sys.argv[2] != "--no-validate":
+        schema_fname = os.path.join(os.path.dirname(__file__), "output_schema.json")
+        with open(schema_fname, "r") as output_schema:
+            validate(link_db, json.load(output_schema))
+
     print(json.dumps(link_db))
 
 
