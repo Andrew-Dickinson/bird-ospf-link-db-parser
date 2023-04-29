4e62c1cf66c14a6cf7175c771b0e2ba558f002f3
Andrew Dickinson <Andrew-Dickinson@users.noreply.github.com>
2023-04-29 00:12:44 -0400

    Add basic stack-based implementation

--- src/bird_parser/parse_bird_output.py
+++ src/bird_parser/parse_bird_output.py
@@ -1,83 +1,138 @@
 import re
+from typing import List
+
 
 def get_indentation_level(line: str):
     return len(line) - len(line.lstrip("\t"))
 
 
+def create_key_from_path(dictionary: dict, path: List[str]):
+    nested_dict = dictionary
+    for item in path:
+        if item not in nested_dict:
+            nested_dict[item] = {}
+        nested_dict = nested_dict[item]
+
+
+def set_value_by_path(dictionary: dict, path: List[str], value=None):
+    nested_dict = dictionary
+    for item in path[:-1]:
+        nested_dict = nested_dict[item]
+
+    nested_dict[path[-1]] = value
+
+
+def get_value_by_path(dictionary: dict, path: List[str]):
+    nested_dict = dictionary
+    for item in path:
+        nested_dict = nested_dict[item]
+
+    return nested_dict
+
+
+def parse_router_link(line_parts: List[str]):
+    assert line_parts[2] in ["metric", "metric2"]
+    link = {"id": line_parts[1], line_parts[2]: int(line_parts[3])}
+
+    if len(line_parts) == 6:
+        assert line_parts[4] == "via"
+        link["via"] = line_parts[5]
+
+    return link
+
+
 def parse_ospf_state_all(ospf_state_str: str):
     lines = [line.rstrip() for line in ospf_state_str.split("\n") if len(line) > 0]
+    lines.append("")
 
     assert re.match(r"BIRD.+ready\.", lines[0])
 
-    areas = {}
+    output = {}
 
-    current_area_id = None
-    current_area = {}
-
-    current_item_id = None
-    current_item = {}
-    current_item_type = None
+    previous_indentation_level = -1
+    path_stack = []
 
     for line in lines[1:]:
         line_parts = line.split()
         indentation_level = get_indentation_level(line)
 
-        if indentation_level == 0:
-            assert len(line_parts) == 2
-            assert line_parts[0] == "area"
+        if line_parts and (
+            line_parts[0] == "distance" or line_parts[0] == "unreachable"
+        ):
+            # Ignore distance/reachability from our local perspective as it's not meaningful to
+            # understanding the global state of the system
+            continue
 
-            if current_area_id is not None:
-                areas[current_area_id] = current_area
-
-            current_area = {"routers": {}, "networks": {}}
-            current_area_id = line_parts[1]
-        elif indentation_level == 1:
-            assert len(line_parts) == 2
-            assert line_parts[0] in ["router", "network"]
-
-            if current_item_id is not None:
-                current_area[f"{current_item_type}s"][current_item_id] = current_item
-
-            current_item_type = line_parts[0]
-            current_item = {"links": {}} if current_item_type == "router" else {}
-            current_item_id = line_parts[1]
-        elif indentation_level == 2:
-            if line_parts[0] == "distance" or line_parts[0] == "unreachable":
-                # Ignore distance/reachability from our local perspective as it's not meaningful to
-                # understanding the global state of the system
-                continue
-
-            if current_item_type == "router":
-                if line_parts[0] == "external":
-                    assert len(line_parts) == 4 or len(line_parts) == 6
+        level_diff = indentation_level - previous_indentation_level
+        if level_diff > 0:
+            assert (
+                level_diff == 1
+            ), "Indenting by more than one tab at a time is not supported"
+            create_key_from_path(output, path_stack)
+            if indentation_level == 1:
+                create_key_from_path(output, path_stack + ["networks"])
+                create_key_from_path(output, path_stack + ["routers"])
+            if indentation_level == 2:
+                if path_stack[2] == "routers":
+                    default_object = {
+                        "links": {
+                            "network": [],
+                            "external": [],
+                            "router": [],
+                            "stubnet": [],
+                        }
+                    }
+                elif path_stack[2] == "networks":
+                    default_object = {
+                        "dr": "",
+                        "routers": [],
+                    }
                 else:
-                    assert len(line_parts) == 4
+                    raise ValueError(f"Invalid object: '{path_stack[2]}' found!")
 
-                resource_name = line_parts[0]
-                assert resource_name in ["router", "stubnet", "external", "network"]
+                set_value_by_path(
+                    output,
+                    path_stack,
+                    default_object,
+                )
+        else:
+            if level_diff < 0:
+                if previous_indentation_level == 2:
+                    if path_stack[2] == "routers":
+                        link_lists = get_value_by_path(
+                            output, path_stack[:-2] + ["links"]
+                        )
+                        lists_to_remove = [
+                            link_type
+                            for link_type, links in link_lists.items()
+                            if len(links) == 0
+                        ]
+                        for link_type in lists_to_remove:
+                            del link_lists[link_type]
+            for i in range(max(-level_diff, 0) + 1):
+                path_stack.pop()
+                path_stack.pop()
 
-                if resource_name not in current_item["links"]:
-                    current_item["links"][resource_name] = []
+        if line_parts:
+            path_stack.append(line_parts[0] + "s")
+            path_stack.append(line_parts[1])
 
-                assert line_parts[2] in ["metric", "metric2"]
-                resource = {"id": line_parts[1], line_parts[2]: int(line_parts[3])}
+            if indentation_level == 2:
+                if path_stack[2] == "routers":
+                    link_type = line_parts[0]
+                    link_list_for_type = get_value_by_path(
+                        output, path_stack[:-2] + ["links", link_type]
+                    )
+                    link_list_for_type.append(parse_router_link(line_parts))
+                elif path_stack[2] == "networks":
+                    network = get_value_by_path(output, path_stack[:-2])
+                    if line_parts[0] == "dr":
+                        network["dr"] = line_parts[1]
+                    elif line_parts[0] == "router":
+                        network["routers"].append(line_parts[1])
+                    else:
+                        raise ValueError(f"Invalid line: {line}")
 
-                if len(line_parts) == 6:
-                    assert line_parts[4] == "via"
-                    resource[line_parts[4]] = line_parts[5]
+        previous_indentation_level = indentation_level
 
-                current_item["links"][resource_name].append(resource)
-            elif current_item_type == "network":
-                assert len(line_parts) == 2
-
-                assert line_parts[0] in ["dr", "router"]
-                if line_parts[0] == "dr":
-                    current_item["dr"] = line_parts[1]
-                elif line_parts[0] == "router":
-                    if "routers" not in current_item:
-                        current_item["routers"] = []
-                    current_item["routers"].append(line_parts[1])
-
-    current_area[f"{current_item_type}s"][current_item_id] = current_item
-    areas[current_area_id] = current_area
-    return {"areas": areas}
+    return output
